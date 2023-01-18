89b2f031a7062459f5863b225b7627fcddf9de9f
SarahAPettigrew <80193606+SarahAPettigrew@users.noreply.github.com>
2023-01-18 21:39:41 +0000

    errors

--- Ex_Files/01_01_begin/hello.py
+++ Ex_Files/01_01_begin/hello.py
@@ -1,1 +1,22 @@
 import this
+
+try:
+    import camelcas
+
+    c = camelcase.CamelCase()
+    txt = 'this is some text'
+    assert isinstance(txt, str)
+    t = [5, 6] * (4,)
+    print(y.hump(txt))
+
+except NameError as e:
+    print(e)
+except ZeroDivisionError as e:
+    print(e)
+except TypeError as e:
+    print(e)
+except ImportError as e:
+    print(e)
+
+finally:
+    print("done")
